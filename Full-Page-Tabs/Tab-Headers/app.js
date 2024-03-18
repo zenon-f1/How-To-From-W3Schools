102b49330c995c1037b2ac3b1f0188beb65b36b4
Vinay Sharma <com.vinay45@gmail.com>
2024-03-18 23:14:33 +0530

    Different Types of Tabs

---
+++ Full-Page-Tabs/Tab-Headers/app.js
@@ -0,0 +1,20 @@
+function openPage(pageName, elmnt, color) {
+
+    let tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
+    }
+
+    tablinks = document.getElementsByClassName('tablink');
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+
+    document.getElementById(pageName).style.display = 'block';
+
+    elmnt.style.backgroundColor = color;
+
+}
+
+document.getElementById('defaultOpen').click();
